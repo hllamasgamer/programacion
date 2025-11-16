4c98f9a16599e912d4fbe2aa353d76e370bde6ce
hllamasgamer <152009173+hllamasgamer@users.noreply.github.com>
2025-11-16 19:34:19 +0100

    Add files via upload

---
+++ actividad1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "");
+    printf("Hola, como te llamas?\n");
+    char nombre[50];
+    scanf("%49s", nombre);
+    printf("De que pais eres?\n");
+    char pais[50];
+    scanf("%49s", pais);
+    if (strcmp(pais, "Japon") ==0){
+
+        printf("hajimemashite,%s-san\n", nombre);
+    }
+    else if (strcmp(pais, "Inglaterra") ==0){
+        printf("Nice to meet you, %s\n", nombre);
+    }
+    else {
+        printf("Mucho gusto, %s\n", nombre);
+    }
+    return 0;
+}
