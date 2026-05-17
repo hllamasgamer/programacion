057b24d0bb09d21b2d4e807a94449df651047348
hllamasgamer <152009173+hllamasgamer@users.noreply.github.com>
2026-05-17 13:29:28 +0200

    Delete C++/Tema 1 directory

--- C++/Tema 1/CalendarioPerpetuo/DiaSemana.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// Enumera los días de la semana, a partir de 0.
-enum DiaSemana {
-	lunes, martes, miercoles, jueves, viernes, sabado, domingo
-};
--- C++/Tema 1/CalendarioPerpetuo/MesAño.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-// Enumera los meses del año, a partir de 0.
-enum MesAño {
-	enero, febrero, marzo, abril, mayo, junio,
-	julio, agosto, setiembre, octubre, noviembre, diciembre
-};
